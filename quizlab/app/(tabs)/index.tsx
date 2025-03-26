32fc67b9eba90aedc374c7fdbcc15b1b403cf16a
Leeseoleem <leeseorim0029@gmail.com>
2025-03-27 00:41:39 +0900

    feat start app

---
+++ quizlab/app/(tabs)/index.tsx
@@ -0,0 +1,32 @@
+import { Image, StyleSheet, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>하이</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
---
+++ quizlab/src/constants/colors.ts
@@ -0,0 +1,18 @@
+export const MainColors = {
+  primary: "#F97316",
+  secondary: "#FFA94D",
+  primaryForeground: "#FDFCF6",
+  danger: "#EF4444",
+  dangerSec: "#FFCFCF",
+  safe: "#34C759",
+  safeSec: "#C0FFD0",
+};
+
+export const GrayColors = {
+  gray40: "#1C160F",
+  gray30: "#7A7672",
+  gray20: "#7A7672",
+  gray10: "#E5E2DB",
+  white: "#FFFFFF",
+  black: "#0A0908",
+};
